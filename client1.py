35381d3cc0ad0e5a27bc8f1750c8db61e8017733
mikekmp <45897741+mikekmp@users.noreply.github.com>
2021-01-16 21:11:25 +0200

    Add files via upload
    Coded in main menu (2 buttons, rainbow background) expanding on code uploaded on last commit.

--- client1.py
+++ client1.py
@@ -4,6 +4,7 @@ import time
 import pygame
 from win32api import GetSystemMetrics
 from random import randrange
+import pygame.freetype
 
 joininggame = False
 disconnected = False
@@ -30,19 +31,27 @@ fullscreen = False
 infoObject = pygame.display.Info()
 w = GetSystemMetrics(0)
 h = GetSystemMetrics(1)
-DISPLAYSURF = pygame.display.set_mode((1000, 600), pygame.RESIZABLE)
+ww = 1000
+nn = 600
+DISPLAYSURF = pygame.display.set_mode((ww, nn), pygame.RESIZABLE)
 keys = pygame.key.get_pressed()
 alt = False
 f4 = False
 mainloop = True
+click = False
 stateofgame = 0
 mouse = pygame.mouse.get_pos()
+font = pygame.freetype.Font("font.ttf", 36)
+font2 = pygame.freetype.Font("font.ttf", 90)
 # FPS = 60  # frames per second setting
 # fpsClock = pygame.time.Clock()
 
 
 def mainmenu():
-    global f4, alt, fullscreen, mainloop, disconnected, DISPLAYSURF, stateofgame, mouse, r, g, b, r3, g3, b3
+    global f4, alt, fullscreen, mainloop, disconnected, DISPLAYSURF, stateofgame, mouse, r, g, b, r3, g3, b3, \
+        w, h, click
+    wn, hn = pygame.display.get_surface().get_size()
+    mx, my = pygame.mouse.get_pos()
     # , FPS, fpsClock
     if r3 == "a":
         r2 = r - randrange(20)
@@ -84,7 +93,49 @@ def mainmenu():
     print(g)
     print(b)
     DISPLAYSURF.fill((r, g, b))
+    if fullscreen:
+        # text on top
+        text_surface, rect = font2.render("MAIN MENU", (0, 0, 0))
+        DISPLAYSURF.blit(text_surface, (int(wn / 2 - 160), 50))
+        # buttons
+        button1 = pygame.draw.rect(DISPLAYSURF, (255, 0, 0), (int(w/2-160), 280, 300, 80))
+        text_surface, rect = font.render("Play", (0, 0, 0))
+        DISPLAYSURF.blit(text_surface, (int(w/2 - 35), 300))
+        if button1.collidepoint(mx, my):
+            if click:
+                stateofgame += 1
+        button2 = pygame.draw.rect(DISPLAYSURF, (255, 0, 0), (int(w / 2 - 160), 420, 300, 80))
+        text_surface, rect = font.render("Exit", (0, 0, 0))
+        DISPLAYSURF.blit(text_surface, (int(w / 2 - 35), 440))
+        if button2.collidepoint(mx, my):
+            if click:
+                mainloop = False
+                print("The whole game got closed")
+    else:
+        # text on top
+        text_surface, rect = font2.render("MAIN MENU", (0, 0, 0))
+        DISPLAYSURF.blit(text_surface, (int(wn / 2 - 160), 20))
+        # buttons
+        button1 = pygame.draw.rect(DISPLAYSURF, (255, 0, 0), (int(wn / 2 - 160), 180, 300, 80))
+        text_surface, rect = font.render("Play", (0, 0, 0))
+        DISPLAYSURF.blit(text_surface, (int(wn / 2 - 35), 200))
+        if button1.collidepoint(mx, my):
+            if click:
+                stateofgame += 1
+        button2 = pygame.draw.rect(DISPLAYSURF, (255, 0, 0), (int(wn / 2 - 160), 320, 300, 80))
+        text_surface, rect = font.render("Exit", (0, 0, 0))
+        DISPLAYSURF.blit(text_surface, (int(wn / 2 - 35), 340))
+        if button2.collidepoint(mx, my):
+            if click:
+                mainloop = False
+                print("The whole game got closed")
+    click = False
     for event in pygame.event.get():
+        if event.type == pygame.VIDEORESIZE:
+            if fullscreen:
+                DISPLAYSURF = pygame.display.set_mode((event.w, event.h), pygame.FULLSCREEN)
+            else:
+                DISPLAYSURF = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
         if event.type == pygame.QUIT:
             print("The whole game got closed")
             mainloop = False
@@ -109,6 +160,10 @@ def mainmenu():
                     pygame.quit()
                     DISPLAYSURF = pygame.display.set_mode((w, h), pygame.FULLSCREEN)
                     fullscreen = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                click = True
+    pygame.display.flip()
     # fpsClock.tick(FPS)
 
 
